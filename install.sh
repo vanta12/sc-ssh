5544200adb95376e93d8580018333ee6de41b34d
localhost <local@host>
2026-08-23 19:35:36 +0000

    pin installer to audited runtime commit

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ case "$AUTOSCRIPT_ROOT" in
     /opt/autoscript) ;;
     *) printf '[ERROR] AUTOSCRIPT_ROOT harus /opt/autoscript\n' >&2; exit 1 ;;
 esac
-REPO_REF="8c5840bfd9a5f009ef73118eb052add87a2e75fc"
+REPO_REF="a3275fc024cfedc38c08afa5f72de7c1ce9ed6f5"
 REPO_RAW_BASE="https://raw.githubusercontent.com/vanta12/sc-ssh/${REPO_REF}"
 RUNTIME_DIR="${AUTOSCRIPT_ROOT}/runtime"
 LIB_DIR="${RUNTIME_DIR}/lib"
